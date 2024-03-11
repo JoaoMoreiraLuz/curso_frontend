53d26e04a5dd2acbe320cabdbc1bcfdc26e92b5f
Joao Gustavo <joaogustavo9992@outlook.com>
2024-03-11 16:20:41 -0300

    quick fix

--- Projeto 2/main.js
+++ Projeto 2/main.js
@@ -48,7 +48,7 @@ function atualizarTabela() {
 function atualizarMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById(`valorMediaFinal`).innerHTML = mediaFinal;
+    document.getElementById(`valorMediaFinal`).innerHTML = mediaFinal.toFixed(2);
     document.getElementById(`mediaFinalResultado`).innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
     console.log(media);
