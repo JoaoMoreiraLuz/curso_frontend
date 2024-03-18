7c3b2608c0e87ed3fca7acbad2859650956e63ce
Joao Gustavo <joaogustavo9992@outlook.com>
2024-03-18 14:45:03 -0300

    exercicio Jquery da EBAC

--- exercicio_jquery/main.js
+++ exercicio_jquery/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $('.starterButton').click(function() {
-        $('div').slidedown()
+    $('#starterButton').click(function() {
+        $('form').slideDown()
     })
+
+    $('#addButton').click(function() {
+        $('.lista').slideDown()
+    })
+    
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const novaTarefa = $('#novaTarefa').val();
+        $('ul').append(`<li><span class="pendente"> </span> ${novaTarefa}</li>`);
+
+        $("ul").on('click', 'li', function() {
+            $(this).toggleClass("completo");
+        })
+    })
+
 })
